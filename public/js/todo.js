f33bf934a4e75688cdfddfc0f3083162cb9b2e2d
Matt Holloway <mholloway@book.com>
2015-11-23 08:56:58 -0500

    Add filter, alter some styling, update data

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,16 +1,24 @@
+//ToDo App
 var TodoApp = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
 				<Header title={this.props.title}/>
-				<div className="row">
-					<div className="col-md-12">
-						<TodoForm />
+				<div className="todoContainer">
+					<div className="row formRow">
+						<div className="col-md-12">
+							<TodoForm />
+						</div>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-12">
-						<TodoList />
+					<div className="row listRow">
+						<div className="col-md-12">
+							<TodoList />
+						</div>
+					</div>
+					<div className="row filterRow">
+						<div className="col-md-12">
+							<TodoFilter  items={2}/>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -18,16 +26,16 @@ var TodoApp = React.createClass({
 	}
 });
 
-// Nav and Title
+//Nav and Title
 var Header = React.createClass({
 	render: function() {
 		return (
-			<h1>{this.props.title}</h1>
+			<h1 className="todoHeader">{this.props.title}</h1>
 		);	
 	}
 });
 
-// ToDo Input Form
+//ToDo Input Form
 var TodoForm = React.createClass({
 	render: function() {
 		return (
@@ -45,7 +53,7 @@ var TodoForm = React.createClass({
 	}
 });
 
-// ToDo List
+//ToDo List
 var TodoList = React.createClass({
 	//TODO: Replace data here with Server data
 	render: function() {
@@ -60,7 +68,21 @@ var TodoList = React.createClass({
 	}
 });
 
+//ToDo Filter
+var TodoFilter = React.createClass({
+	render: function(){
+		return (
+			<div className="todoFilter">
+				<span className="itemCount">{this.props.items} items left </span>
+				<button type="button" className="btn btn-default allButton"  name="allButton">All</button>
+				<button className="btn btn-link activeButton" name="activeButton">Active</button>
+				<button className="btn btn-link completeButton" name="completeButton">Completed</button>
+			</div>	
+		);
+	}
+});
 
+//Render Page
 ReactDOM.render(
 	<TodoApp title="React TODO" />,
 	document.getElementById('content')
